573500c727f61b51c87555532ae4a2052c21a3f4
oxalica <oxalicc@pm.me>
2023-04-04 23:37:01 +0800

    Impl catch-all handlers for `Router`

--- src/router.rs
+++ src/router.rs
@@ -11,16 +11,23 @@ use tower_service::Service;
 
 use crate::{AnyNotification, AnyRequest, Error, JsonValue, LspService, ResponseError, Result};
 
-#[derive(Default)]
 pub struct Router<S> {
     state: S,
     req_handlers: HashMap<&'static str, BoxReqHandler<S>>,
     notif_handlers: HashMap<&'static str, BoxNotifHandler<S>>,
+    unhandled_req: BoxReqHandler<S>,
+    unhandled_notif: BoxNotifHandler<S>,
 }
 
 type BoxReqFuture = Pin<Box<dyn Future<Output = Result<JsonValue, ResponseError>> + Send>>;
 type BoxReqHandler<S> = Box<dyn Fn(&mut S, AnyRequest) -> BoxReqFuture>;
 type BoxNotifHandler<S> = Box<dyn Fn(&mut S, AnyNotification) -> ControlFlow<Result<()>>>;
+
+impl<S: Default> Default for Router<S> {
+    fn default() -> Self {
+        Self::new(S::default())
+    }
+}
 
 impl<S> Router<S> {
     pub fn new(state: S) -> Self {
@@ -28,6 +35,23 @@ impl<S> Router<S> {
             state,
             req_handlers: HashMap::new(),
             notif_handlers: HashMap::new(),
+            unhandled_req: Box::new(|_, req| {
+                Box::pin(ready(Err(ResponseError {
+                    code: ErrorCode::MethodNotFound as _,
+                    message: format!("No such method {}", req.method),
+                    data: None,
+                })))
+            }),
+            unhandled_notif: Box::new(|_, notif| {
+                if notif.method.starts_with("$/") {
+                    ControlFlow::Continue(())
+                } else {
+                    ControlFlow::Break(Err(Error::Protocol(format!(
+                        "Unhandled notification: {}",
+                        notif.method,
+                    ))))
+                }
+            }),
         }
     }
 
@@ -38,6 +62,7 @@ impl<S> Router<S> {
     where
         Fut: Future<Output = Result<R::Result, ResponseError>> + Send + 'static,
     {
+        self.unhandled_req = Box::new(|_, _| todo!());
         self.req_handlers.insert(
             R::METHOD,
             Box::new(
@@ -74,6 +99,25 @@ impl<S> Router<S> {
         );
         self
     }
+
+    pub fn unhandled_request<Fut>(
+        &mut self,
+        handler: impl Fn(&mut S, AnyRequest) -> Fut + 'static,
+    ) -> &mut Self
+    where
+        Fut: Future<Output = Result<JsonValue, ResponseError>> + Send + 'static,
+    {
+        self.unhandled_req = Box::new(move |service, req| Box::pin(handler(service, req)));
+        self
+    }
+
+    pub fn unhandled_notification(
+        &mut self,
+        handler: impl Fn(&mut S, AnyNotification) -> ControlFlow<Result<()>> + 'static,
+    ) -> &mut Self {
+        self.unhandled_notif = Box::new(handler);
+        self
+    }
 }
 
 impl<S> Service<AnyRequest> for Router<S> {
@@ -86,25 +130,20 @@ impl<S> Service<AnyRequest> for Router<S> {
     }
 
     fn call(&mut self, req: AnyRequest) -> Self::Future {
-        match self.req_handlers.get(&*req.method) {
-            Some(h) => h(&mut self.state, req),
-            None => Box::pin(ready(Err(ResponseError {
-                code: ErrorCode::MethodNotFound as _,
-                message: format!("No such method {}", req.method),
-                data: None,
-            }))),
-        }
+        let h = self
+            .req_handlers
+            .get(&*req.method)
+            .unwrap_or(&self.unhandled_req);
+        h(&mut self.state, req)
     }
 }
 
 impl<S> LspService for Router<S> {
     fn notify(&mut self, notif: AnyNotification) -> ControlFlow<Result<()>> {
-        match self.notif_handlers.get(&*notif.method) {
-            Some(h) => h(&mut self.state, notif),
-            None => ControlFlow::Break(Err(Error::Protocol(format!(
-                "Unhandled notification {}",
-                notif.method
-            )))),
-        }
+        let h = self
+            .notif_handlers
+            .get(&*notif.method)
+            .unwrap_or(&self.unhandled_notif);
+        h(&mut self.state, notif)
     }
 }
