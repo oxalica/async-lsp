20608234476a1bbd5f3e1ac2ae58a76d2707459c
oxalica <oxalicc@pm.me>
2023-04-06 00:02:55 +0800

    Rework Pipe{Stdin,Stdout}

--- examples/stdio.rs
+++ examples/stdio.rs
@@ -1,6 +1,40 @@
+use std::time::Duration;
+
+use async_lsp::stdio::{PipeStdin, PipeStdout};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::time::timeout;
+
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
-    let mut stdin = async_lsp::stdio::PipeStdin::lock_forever().unwrap();
-    let mut stdout = async_lsp::stdio::PipeStdout::lock_forever().unwrap();
-    tokio::io::copy(&mut stdin, &mut stdout).await.unwrap();
+async fn main() -> std::io::Result<()> {
+    eprintln!("Non blocking");
+    {
+        let mut stdin = PipeStdin::lock()?;
+        let mut stdout = PipeStdout::lock()?;
+        let mut buf = [0u8; 4 << 10];
+        let mut i = 0u64;
+        loop {
+            match timeout(Duration::from_secs(1), stdin.read(&mut buf)).await {
+                Ok(ret) => {
+                    let len = ret?;
+                    stdout.write_all(&buf[..len]).await?;
+                    if buf[..len].contains(&b'x') {
+                        break;
+                    }
+                }
+                Err(_) => {
+                    i += 1;
+                    stdout.write_all(i.to_string().as_bytes()).await?;
+                }
+            }
+        }
+    }
+
+    eprintln!("Blocking");
+    {
+        let mut buf = String::new();
+        std::io::stdin().read_line(&mut buf)?;
+        println!("{buf:?}");
+    }
+
+    Ok(())
 }
--- src/stdio.rs
+++ src/stdio.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
-use std::io::{self, Result};
-use std::mem::{self, ManuallyDrop};
+use std::io::{self, Error, ErrorKind, IoSlice, Result, StdinLock, StdoutLock};
+use std::mem::ManuallyDrop;
 use std::os::fd::{AsRawFd, FromRawFd};
+use std::os::unix::prelude::FileTypeExt;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
@@ -9,20 +10,62 @@ use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 use tokio::net::unix::pipe;
 
 #[derive(Debug)]
+struct NonBlocking<T: AsRawFd> {
+    inner: T,
+    prev_flags: libc::c_int,
+}
+
+impl<T: AsRawFd> NonBlocking<T> {
+    unsafe fn new(inner: T) -> Result<Self> {
+        let fd = inner.as_raw_fd();
+        let ft = ManuallyDrop::new(File::from_raw_fd(fd))
+            .metadata()?
+            .file_type();
+        if !(ft.is_fifo() || ft.is_socket() || ft.is_char_device()) {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("File with mode {ft:?} is not pipe-like"),
+            ));
+        }
+
+        let flags = unsafe { libc::fcntl(fd, libc::F_GETFL) };
+        if flags < 0 || libc::fcntl(inner.as_raw_fd(), libc::F_SETFL, flags | libc::O_NONBLOCK) != 0
+        {
+            return Err(Error::last_os_error());
+        }
+        Ok(Self {
+            inner,
+            prev_flags: flags,
+        })
+    }
+}
+
+impl<T: AsRawFd> Drop for NonBlocking<T> {
+    fn drop(&mut self) {
+        unsafe {
+            let _ = libc::fcntl(self.inner.as_raw_fd(), libc::F_SETFL, self.prev_flags);
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct PipeStdin {
     inner: pipe::Receiver,
+    // NB. `_lock` is dropped after `inner` closes the duplicate.
+    // So that we can safely reset the flags.
+    _lock: NonBlocking<StdinLock<'static>>,
 }
 
 impl PipeStdin {
-    pub fn lock_forever() -> Result<Self> {
-        let fd = io::stdin().as_raw_fd();
-        mem::forget(io::stdin().lock());
+    pub fn lock() -> Result<Self> {
+        let lock = unsafe { NonBlocking::new(io::stdin().lock())? };
         // We must not close the stdin, but we have to pass an owned `File` into tokio.
         // Here we dup the file descripTOR to make it sound.
         // Note that the duplicate shares the same file descriptION.
-        let file = unsafe { ManuallyDrop::new(File::from_raw_fd(fd)).try_clone()? };
-        let inner = pipe::Receiver::from_file(file)?;
-        Ok(PipeStdin { inner })
+        let file =
+            unsafe { ManuallyDrop::new(File::from_raw_fd(lock.inner.as_raw_fd())).try_clone()? };
+        let inner = pipe::Receiver::from_file_unchecked(file)?;
+        Ok(Self { inner, _lock: lock })
     }
 }
 
@@ -31,7 +74,7 @@ impl AsyncRead for PipeStdin {
         mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
-    ) -> Poll<io::Result<()>> {
+    ) -> Poll<Result<()>> {
         Pin::new(&mut self.inner).poll_read(cx, buf)
     }
 }
@@ -39,16 +82,19 @@ impl AsyncRead for PipeStdin {
 #[derive(Debug)]
 pub struct PipeStdout {
     inner: pipe::Sender,
+    // NB. `_lock` is dropped after `inner` closes the duplicate.
+    // See `PipeStdin`.
+    _lock: NonBlocking<StdoutLock<'static>>,
 }
 
 impl PipeStdout {
-    pub fn lock_forever() -> Result<Self> {
-        let fd = io::stdout().as_raw_fd();
-        mem::forget(io::stdout().lock());
-        // See `PipeStdin::lock_forever`.
-        let file = unsafe { ManuallyDrop::new(File::from_raw_fd(fd)).try_clone()? };
-        let inner = pipe::Sender::from_file(file)?;
-        Ok(PipeStdout { inner })
+    pub fn lock() -> Result<Self> {
+        let lock = unsafe { NonBlocking::new(io::stdout().lock())? };
+        // See `PipeStdin::lock`.
+        let file =
+            unsafe { ManuallyDrop::new(File::from_raw_fd(lock.inner.as_raw_fd())).try_clone()? };
+        let inner = pipe::Sender::from_file_unchecked(file)?;
+        Ok(Self { inner, _lock: lock })
     }
 }
 
@@ -57,15 +103,27 @@ impl AsyncWrite for PipeStdout {
         mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &[u8],
-    ) -> Poll<io::Result<usize>> {
+    ) -> Poll<Result<usize>> {
         Pin::new(&mut self.inner).poll_write(cx, buf)
     }
 
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<()>> {
         Pin::new(&mut self.inner).poll_flush(cx)
     }
 
-    fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+    fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<()>> {
         Pin::new(&mut self.inner).poll_shutdown(cx)
+    }
+
+    fn poll_write_vectored(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[IoSlice<'_>],
+    ) -> Poll<Result<usize>> {
+        Pin::new(&mut self.inner).poll_write_vectored(cx, bufs)
+    }
+
+    fn is_write_vectored(&self) -> bool {
+        self.inner.is_write_vectored()
     }
 }
