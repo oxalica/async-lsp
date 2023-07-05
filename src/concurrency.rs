87777078c7155cc05224923ec10c7c835772c558
oxalica <oxalicc@pm.me>
2023-07-05 09:32:32 +0800

    Refactor `Concurrency`
    - Avoid polling each ongoing requests every time.
    - Get rid of asynchronous channels.

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -7,17 +7,18 @@
 //! 1. Limit concurrent incoming requests to at most `max_concurrency`.
 //! 2. Cancellation of incoming requests via client notification `$/cancelRequest`.
 use std::collections::HashMap;
-use std::convert::Infallible;
 use std::future::Future;
 use std::num::NonZeroUsize;
 use std::ops::ControlFlow;
 use std::pin::Pin;
-use std::task::{ready, Context, Poll};
+use std::sync::{Arc, Weak};
+use std::task::{Context, Poll};
 use std::thread::available_parallelism;
 
+use futures::stream::{AbortHandle, Abortable};
+use futures::task::AtomicWaker;
 use lsp_types::notification::{self, Notification};
 use pin_project_lite::pin_project;
-use tokio::sync::oneshot;
 use tower_layer::Layer;
 use tower_service::Service;
 
@@ -32,7 +33,9 @@ use crate::{
 pub struct Concurrency<S> {
     service: S,
     max_concurrency: NonZeroUsize,
-    ongoing: HashMap<RequestId, oneshot::Sender<Infallible>>,
+    /// A specialized single-acquire-multiple-release semaphore, using `Arc::weak_count` as tokens.
+    semaphore: Arc<AtomicWaker>,
+    ongoing: HashMap<RequestId, AbortHandle>,
 }
 
 impl<S: LspService> Service<AnyRequest> for Concurrency<S> {
@@ -41,20 +44,69 @@ impl<S: LspService> Service<AnyRequest> for Concurrency<S> {
     type Future = ResponseFuture<S::Future>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.ongoing.retain(|_, tx| tx.poll_closed(cx).is_pending());
-        if self.ongoing.len() < self.max_concurrency.get() {
-            Poll::Ready(Ok(()))
-        } else {
-            Poll::Pending
+        if Arc::weak_count(&self.semaphore) >= self.max_concurrency.get() {
+            // Implicit `Acquire`.
+            self.semaphore.register(cx.waker());
+            // No guards dropped between the check and register?
+            if Arc::weak_count(&self.semaphore) >= self.max_concurrency.get() {
+                return Poll::Pending;
+            }
         }
+
+        // Here we have `weak_count < max_concurrency`. The service is ready for new calls.
+        Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, req: AnyRequest) -> Self::Future {
-        assert!(self.ongoing.len() < self.max_concurrency.get(), "Not ready");
-        let (cancel_tx, cancel_rx) = oneshot::channel();
-        self.ongoing.insert(req.id.clone(), cancel_tx);
+        let guard = SemaphoreGuard(Arc::downgrade(&self.semaphore));
+        debug_assert!(
+            Arc::weak_count(&self.semaphore) <= self.max_concurrency.get(),
+            "`poll_ready` is not called before `call`",
+        );
+
+        let (handle, reg) = AbortHandle::new_pair();
+
+        // Regularly purge completed or dead tasks. See also `AbortOnDrop` below.
+        // This costs 2*N time to remove at least N tasks, results in amortized O(1) time cost
+        // for each spawned task.
+        if self.ongoing.len() >= self.max_concurrency.get() * 2 {
+            self.ongoing.retain(|_, handle| !handle.is_aborted());
+        }
+        self.ongoing.insert(req.id.clone(), handle.clone());
+
         let fut = self.service.call(req);
-        ResponseFuture { fut, cancel_rx }
+        let fut = Abortable::new(fut, reg);
+        ResponseFuture {
+            fut,
+            _abort_on_drop: AbortOnDrop(handle),
+            _guard: guard,
+        }
+    }
+}
+
+struct SemaphoreGuard(Weak<AtomicWaker>);
+
+impl Drop for SemaphoreGuard {
+    fn drop(&mut self) {
+        if let Some(sema) = self.0.upgrade() {
+            if let Some(waker) = sema.take() {
+                // Return the token first.
+                drop(sema);
+                // Wake up `poll_ready`. Implicit "Release".
+                waker.wake()
+            }
+        }
+    }
+}
+
+/// By default, the `AbortHandle` only transfers information from it to `Abortable<_>`, not in
+/// reverse. But we want to set the flag on drop (either success or failure), so that the `ongoing`
+/// map can be purged regularly without bloating indefinitely.
+struct AbortOnDrop(AbortHandle);
+
+impl Drop for AbortOnDrop {
+    fn drop(&mut self) {
+        self.0.abort();
     }
 }
 
@@ -62,8 +114,11 @@ pin_project! {
     /// The [`Future`] type used by the [`Concurrency`] middleware.
     pub struct ResponseFuture<Fut> {
         #[pin]
-        fut: Fut,
-        cancel_rx: oneshot::Receiver<Infallible>,
+        fut: Abortable<Fut>,
+        // NB. Comes before `SemaphoreGuard`. So that when the guard wake up the caller, it is able
+        // to purge the current future from `ongoing` map immediately.
+        _abort_on_drop: AbortOnDrop,
+        _guard: SemaphoreGuard,
     }
 }
 
@@ -71,13 +126,10 @@ impl<Fut: Future<Output = Result<JsonValue, ResponseError>>> Future for Response
     type Output = Fut::Output;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let this = self.project();
-        if let Poll::Ready(ret) = this.fut.poll(cx) {
-            return Poll::Ready(ret);
-        }
-        match ready!(Pin::new(this.cancel_rx).poll(cx)) {
-            Ok(never) => match never {},
-            Err(_) => Poll::Ready(Err(ResponseError {
+        match self.project().fut.poll(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(Ok(inner_ret)) => Poll::Ready(inner_ret),
+            Poll::Ready(Err(_aborted)) => Poll::Ready(Err(ResponseError {
                 code: ErrorCode::REQUEST_CANCELLED,
                 message: "Client cancelled the request".into(),
                 data: None,
@@ -137,7 +189,14 @@ impl<S> Layer<S> for ConcurrencyBuilder {
         Concurrency {
             service: inner,
             max_concurrency: self.max_concurrency,
-            ongoing: HashMap::with_capacity(self.max_concurrency.get()),
+            semaphore: Arc::new(AtomicWaker::new()),
+            // See `Concurrency::call` for why the factor 2.
+            ongoing: HashMap::with_capacity(
+                self.max_concurrency
+                    .get()
+                    .checked_mul(2)
+                    .expect("max_concurrency overflow"),
+            ),
         }
     }
 }
